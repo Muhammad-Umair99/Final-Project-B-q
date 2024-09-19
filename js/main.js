13fb1fb538ca00d653d9b4f39f1e5fed4f974351
Muhammad-Umair99 <muhammadumair10010@gmail.com>
2024-09-19 19:30:25 +0500

    make sign up & sign in & correct the name of index file

--- js/main.js
+++ js/main.js
@@ -16,3 +16,53 @@ if(navClose){
         nav.classList.remove("show_menu")
     })
 }
+
+
+// Sign-up/Sign-in functionality
+const signUpBtn = document.querySelector(".signupbtn");
+const signInBtn = document.querySelector(".signinbtn");
+const nameField = document.querySelector(".namefield");
+const title = document.querySelector(".title");
+const underline = document.querySelector(".underline");
+let text = document.querySelector(".text");
+const accountLink = document.querySelector(".ri-account-circle-line"); // Account icon
+const authContainer = document.getElementById("auth-container"); // The form container
+
+// Show the authentication form when the account icon is clicked
+accountLink.addEventListener("click", () => {
+    authContainer.style.display = 'flex'; // Show the form
+    backArrow.style.display = 'flex'
+});
+
+// Handle Sign In functionality
+signInBtn.addEventListener("click", () => {
+    nameField.style.maxHeight = "0"; // Hide name field for sign in
+    title.innerHTML = "Sign In";
+    text.innerHTML = "Lost Password"
+    signUpBtn.classList.add("disable");
+    signInBtn.classList.remove("disable");
+    underline.style.transform = "translateX(35px)"; // Move underline
+});
+
+// Handle Sign Up functionality
+signUpBtn.addEventListener("click", () => {
+    nameField.style.maxHeight = "60px"; // Show name field for sign up
+    title.innerHTML = "Sign Up";
+    text.innerHTML = "Password Suggestion"
+    signUpBtn.classList.remove("disable");
+    signInBtn.classList.add("disable");
+    underline.style.transform = "translateX(0)"; // Reset underline
+});
+
+// JavaScript for back arrow navigation
+const backArrow = document.getElementById("back-arrow");
+
+// Navigate to the previous page on click
+if (backArrow) {
+    backArrow.addEventListener("click", () => {
+        window.location.href = "index.html"; // Change to your actual dashboard URL
+    });
+}
+
+
+
